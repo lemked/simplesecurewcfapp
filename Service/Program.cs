551d126746e29405e2d7a1452b640032abfe6f7a
Daniel Lemke <lemked@web.de>
2014-12-28 18:10:08 +0100

    Implemented service

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,19 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(WcfService)))
+            {
+                var address = new Uri("https://localhost:8999/MyService");
+                var binding = new BasicHttpBinding();
+                host.AddServiceEndpoint(typeof(IWcfService), binding, address);
 
+                host.Open();
+
+                Console.WriteLine("Service started, press any key to finish execution.");
+                Console.ReadKey();
+
+                host.Close();
+            }
         }
     }
 }
---
+++ Service/WcfService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service
+{
+    public class WcfService : IWcfService
+    {
+        public string Echo(string input)
+        {
+            return String.Format("You've entered: {0}", input);
+        }
+    }
+}
